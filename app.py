cd6a155879ef2c6058bbdb34581407ad163f1098
ShibenaLabs <121632876+ShibenaLabs@users.noreply.github.com>
2023-01-31 13:56:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def init():
     global model
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
     
-    repo_id = "OfficialUnstableDiffusion/UnstablePhotoRealv.5"
+    repo_id = "Linaqruf/anything-v3.0"
 
     scheduler = EulerDiscreteScheduler.from_pretrained(repo_id, subfolder="scheduler", prediction_type="v_prediction")
 
